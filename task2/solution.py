26ef05fb625e3e01752eac7954211f02c353bb8d
Desislava Petrova <d.panchova.petrova@gmail.com>
2013-04-21 15:46:12 +0300

    Task2: PEP8 check , no more absent or additional whitespaces

--- task2/solution.py
+++ task2/solution.py
@@ -1,33 +1,33 @@
 from collections import defaultdict
+from collections import OrderedDict
+import functools
+import itertools
 
-def groupby(func,seq):
+
+def groupby(func, seq):
     res = defaultdict(list)
     [res[func(x)].append(x) for x in seq]
     return res
 
-import functools
 
 def iterate(func):
     key = lambda x: x
-    while True :
+    while True:
         yield key
-        key = functools.partial(lambda f,*args: func(f(*args)), key)
+        key = functools.partial(lambda f, *args: func(f(*args)), key)
 
-import itertools
 
 def zip_with(func, *iterables):
-    iterators = list(map(iter,iterables))
+    iterators = list(map(iter, iterables))
     return [func(*tuple([next(iterator) for iterator in iterators]))
-            for i in range(min(map(len,list(iterables))))]
+            for i in range(min(map(len, list(iterables))))]
 
-from collections import OrderedDict
 
-def cache(func , cache_size):
-    def func_cached(x , d = OrderedDict()):
+def cache(func, cache_size):
+    def func_cached(x, d=OrderedDict()):
         if x not in d.keys():
-            if len(d) >= cache_size :
+            if len(d) >= cache_size:
                 d.popitem(False)
             d[x] = func(x)
         return d[x]
     return lambda x: func_cached(x)
-
