3bcae367d840b340039b5347c7cfd8ed64a95ff7
Desislava Petrova <d.panchova.petrova@gmail.com>
2013-04-25 21:56:00 +0300

    Task4: use constants

--- task4/solution.py
+++ task4/solution.py
@@ -18,6 +18,17 @@ class NotYourTurn(Exception):
 
 
 class TicTacToeBoard(object):
+    ROWS = "321"
+    COLS = "ABC"
+    VALUES = ['O', 'X']
+    GAME_IN_PROGRESS = 'Game in progress.'
+    DRAW = 'Draw!'
+    X_WINS = 'X wins!'
+    O_WINS = 'O wins!'
+    WIN_LINES = [["A3", "B3", "C3"], ["A2", "B2", "C2"], ["A1", "B1", "C1"],
+                 ["A3", "A2", "A1"], ["B3", "B2", "B1"], ["C3", "C2", "C1"],
+                 ["A3", "B2", "C1"], ["A1", "B2", "C3"]]
+
     def __init__(self):
         self.board = dict()
         self.turn = None
@@ -25,17 +36,17 @@ class TicTacToeBoard(object):
 
     def __str__(self):
         board_str = ""
-        for row in [3, 2, 1]:
+        for row in self.ROWS:
             board_str += "\n  -------------\n"
             board_str += str(row)+" |"
-            for col in "ABC":
+            for col in self.COLS:
                 key = col+str(row)
                 board_str += " "+self.board.get(key, " ")+" |"
         board_str += "\n  -------------\n    A   B   C  \n"
         return board_str
 
     def __setitem__(self, key, value):
-        if value != 'O' and value != 'X':
+        if value not in self.VALUES:
             raise InvalidValue("Use 'X' or 'O' for value.")
 
         if key not in ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]:
@@ -51,22 +62,19 @@ class TicTacToeBoard(object):
 
         self.board[key] = value
 
-        if self.status == "Game in progress." and 4 < len(self.board):
+        if self.status == self.GAME_IN_PROGRESS and 4 < len(self.board):
             if 9 == len(self.board):
-                self.status = 'Draw!'
+                self.status = self.DRAW
             if self.wins(self.board, 'X'):
-                self.status = 'X wins!'
+                self.status = self.X_WINS
             if self.wins(self.board, 'O'):
-                self.status = 'O wins!'
+                self.status = self.O_WINS
 
     def game_status(self):
         return self.status
 
     def wins(self, board, sign):
-        winLines = [["A3", "B3", "C3"], ["A2", "B2", "C2"], ["A1", "B1", "C1"],
-                    ["A3", "A2", "A1"], ["B3", "B2", "B1"], ["C3", "C2", "C1"],
-                    ["A3", "B2", "C1"], ["A1", "B2", "C3"]]
-        values = [[board.get(l) == sign for l in line] for line in winLines]
+        values = [[board.get(l) == sign for l in line] for line in self.WIN_LINES]
         wins = [reduce(lambda x, y: x and y, value) for value in values]
         winer = reduce(lambda x, y: x or y, wins)
         return winer
