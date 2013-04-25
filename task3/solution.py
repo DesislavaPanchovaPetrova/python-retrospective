f9719efd3794e2d17beac85fac36e130b362d75f
Desislava Petrova <d.panchova.petrova@gmail.com>
2013-04-25 21:22:56 +0300

    Task3 : use list of successors instead of set

--- task3/solution.py
+++ task3/solution.py
@@ -2,7 +2,7 @@ class Person(object):
 
     def __init__(self, name, birth_year, gender, father=None, mother=None):
         self.__siblings_observers = []
-        self.successors = set()
+        self.successors = []
         self.name = name
         self.birth_year = birth_year
         self.gender = gender
@@ -10,10 +10,10 @@ class Person(object):
         self.mother = mother
         if mother is not None:
             self.__siblings_observers.append(mother.children)
-            mother.successors.add(self)
+            mother.successors.append(self)
         if father is not None:
             self.__siblings_observers.append(father.children)
-            father.successors.add(self)
+            father.successors.append(self)
 
     def get_brothers(self):
         return self.__get_my_siblings("M")
@@ -30,10 +30,9 @@ class Person(object):
 
     def children(self, gender="both"):
         if gender == "both":
-            return list(self.successors)
+            return self.successors
         else:
-            return list(filter(lambda child: child.gender == gender,
-                               list(self.successors)))
+            return [child for child in self.successors if child.gender == gender]
 
     def is_direct_successor(self, other):
         return other in self.successors or self in other.successors
